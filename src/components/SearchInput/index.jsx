fd4d669391af896213821be54e27c9baa2b66cf6
felipedevpips-max <felipedevpips@gmail.com>
2026-01-13 13:50:37 -0300

    aplicando css modules

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -6,7 +6,7 @@ export const SearchInput = (props) => {
     return (
         <div className={styles.container}>
             <IconSearch/>
-            <input className={styles.input} {...props}/>
+            <input placeholder='Procure seu dinheiro...' className={styles.input} {...props}/>
         </div>
     )
 }
