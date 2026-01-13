18e176a71b52143267c85392163cfb7a37b12245
felipedevpips-max <felipedevpips@gmail.com>
2026-01-13 09:37:01 -0300

    continuando

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Aside } from "./components/Aside";
+import { Container } from "./components/Container";
+
+function App() {
+  return (
+    <Container>
+      <Aside />
+      <h1>eae</h1>
+    </Container>
+  );
+}
+
+export default App;
