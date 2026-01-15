ae72fbb7a8ec5ec2972c58bfe87b78461e1d5e41
felipedevpips-max <felipedevpips@gmail.com>
2026-01-15 15:10:58 -0300

    aplicando grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+
 import { Aside } from "./components/Aside";
 import { Container } from "./components/Container";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
 import { Typography } from "./components/Typography";
 import { DailyBudget } from "./components/DailyBudget";
+import styles from './components/app.module.css'
 
 import Card from "./components/Card";
 import { SavingStatus } from "./components/SavingsStatus";
@@ -13,14 +15,15 @@ function App() {
     <Container>
       <Aside />
       <Main>
+        <div className={styles.container}>
         <SearchInput />
-        <div>
+          <div >
           <Typography variant="h1">Olá, Felipe!</Typography>
           <Typography variant="body">
             Veja como estão suas finanças hoje.
           </Typography>
         </div>
-        <section>
+        <section className={styles.grid}>
           <Card>
             <Card.Header>Orçamento diário disponivel:</Card.Header>
             <Card.Body>
@@ -35,7 +38,23 @@ function App() {
             </Card.Body>
           </Card>
 
+          <Card>
+            <Card.Header>Progresso da meta financeira</Card.Header>
+            <Card.Body>
+              <SavingStatus percent={40}/>
+            </Card.Body>
+          </Card>
+
+          <Card>
+            <Card.Header>Progresso da meta financeira</Card.Header>
+            <Card.Body>
+              <SavingStatus percent={40}/>
+            </Card.Body>
+          </Card>
+
         </section>
+        </div>
+        
       </Main>
     </Container>
   );
